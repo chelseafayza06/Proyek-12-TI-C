dddede5577454e48ed1328db16c0aeb5d4e725e2
chelseafayza06 <chelseafayza06@gmail.com>
2026-05-30 15:04:30 +0700

    update komentar

--- tree.py
+++ tree.py
@@ -1,19 +1,29 @@
-class TreeNode:
+# =========================================
+# TREE
+# Digunakan untuk merepresentasikan struktur
+# hierarki penjara dari tingkat tertinggi
+# hingga tingkat terendah.
+# =========================================
+
+class TreeNode: # membuat satu node pada Tree
 
     def __init__(self, nama):
 
-        self.nama = nama
+        self.nama = nama  # menyimpan nama node
 
-        self.child1 = None
-        self.child2 = None
+        self.child1 = None # anak/cabang pertama
+        self.child2 = None  # anak/cabang kedua
 
-
+# Fungsi rekursif untuk menampilkan tree
 def tampil_tree(node, level=0):
 
-    if node is not None:
-
+    if node is not None: # Jika node masih ada
+        
+        # Menampilkan nama node sesuai level
         print("  " * level + node.nama)
 
+        # Menampilkan cabang kiri
         tampil_tree(node.child1, level + 1)
 
+        # Menampilkan cabang kanan
         tampil_tree(node.child2, level + 1)
